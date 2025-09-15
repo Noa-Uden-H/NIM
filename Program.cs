be650330b4ac6fb4a724b284663f290d43994d35
Rasmus Kolling <rasmuskolling26@gmail.com>
2025-09-15 10:15:30 +0200

    Fixed merge 2

--- Program.cs
+++ Program.cs
@@ -15,8 +15,24 @@ namespace NIM
         
         static void Main(string[] args)
         {
-            
-        }
+            int antalbrikker = 15; // Random antal?
+
+            while (true)
+            {
+                Random rnd = new Random();
+
+                for (int i = 1; i != antalbrikker; i++)
+                {
+                    int colornumber = rnd.Next(9, 13);
+                    ConsoleColor color = (ConsoleColor)colornumber;
+                    Console.ForegroundColor = color;
+
+                    Console.Write("O");
+                }
+                Console.ResetColor();
+
+                break;
+            }
 
         static void skiftspiller()
         {
@@ -36,23 +52,6 @@ namespace NIM
             int remove = Convert.ToInt16(Console.ReadLine());
 
             return remove;
-            int antalbrikker = 15; // Random antal?
-
-            while (true)
-            {
-                Random rnd = new Random();
-
-                for (int i = 1; i != antalbrikker; i++)
-                {
-                    int colornumber = rnd.Next(9, 13);
-                    ConsoleColor color = (ConsoleColor)colornumber;
-                    Console.ForegroundColor = color;
-
-                    Console.Write("O");
-                }
-                Console.ResetColor();
-
-                break;
             }
         }
     }
