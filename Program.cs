48e5b53478772694c2cebb8af5d3a27b1343c0dc
Rasmus Kolling <rasmuskolling26@gmail.com>
2025-09-15 10:11:07 +0200

    Created skiftspiller and knapper

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace NIM
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        static void skiftspiller()
+        {
+            if (Globals.spiller = '1')
+            {
+                Globals.spiller = '2';
+            }
+            else
+            {
+                Globals.spiller = '1';
+            }
+        }
+
+        static int knapper()
+        { 
+            Console.Write($"spiller {Globals.spiller} - tag 1/2/3 brikker");
+            int remove = Convert.ToInt16(Console.ReadLine());
+
+            return remove;
         }
     }
 }
